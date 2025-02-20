faefc1a6d0e42b478bdc669c59dd1626a94e55a1
BellRoad <bellroad@gmail.com>
2025-02-20 09:37:54 +0900

    msedgedriver.exe 파일 경로 수정 G:\Study\python -> G:\Study\ToyProject

--- WMS조회/wms_sales_down_xls2xlsx.py
+++ WMS조회/wms_sales_down_xls2xlsx.py
@@ -65,7 +65,7 @@ options = webdriver.EdgeOptions()
 options.add_argument("disable-gpu")
 options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver_path = "G:\Study\python\msedgedriver.exe"
+driver_path = "G:\Study\ToyProject\msedgedriver.exe"
 service = Service(executable_path=driver_path)
 driver = webdriver.Edge(service=service, options=options)
 
